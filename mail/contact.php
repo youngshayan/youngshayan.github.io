f59c1445565589dec4543844e7b910847209309b
YOUNGSHAYAN <92734199+youngshayan@users.noreply.github.com>
2025-05-09 19:57:18 +0330

    change few things
    new email service and reduce pic quality

--- mail/contact.php
+++ mail/contact.php
@@ -1,7 +1,12 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if (
+    empty($_POST['name']) ||
+    empty($_POST['subject']) ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
+    http_response_code(400); // Bad Request
+    exit("Invalid form data.");
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -9,12 +14,24 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "theyoungshayan@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$to = "theyoungshayan@gmail.com";
+$subject = "$m_subject: $name";
+$body = "You have received a new message from your website contact form.\n\n".
+        "Here are the details:\n\n".
+        "Name: $name\n".
+        "Email: $email\n".
+        "Subject: $m_subject\n".
+        "Message: $message";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+$header = "From: no-reply@yourdomain.com\r\n"; // Use a verified domain email
+$header .= "Reply-To: $email\r\n";
+
+// Send the email
+if (!mail($to, $subject, $body, $header)) {
+    http_response_code(500);
+    exit("Mail sending failed.");
+}
+
+http_response_code(200);
+echo "Message sent successfully.";
 ?>
